d63da6ca9923e7c06a1511196c42c2492626e599
TheInfamousGrim <george@grimfunky.dev>
2025-02-10 05:09:46 +0000

    feat(prettier): install and configure prettier
    Also format files using prettier

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,13 +1,15 @@
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-import Page from '../app/page'
+import Page from '../app/page';
 
 test('Page', async () => {
-  render(<Page />)
-  const saveChangesListItem = screen.getByTestId('save-changes')
+  render(<Page />);
+  const saveChangesListItem = screen.getByTestId('save-changes');
 
   // Check the following text content is present:
   // Save and see your changes instantly.
-  expect(saveChangesListItem.textContent).toBe('Save and see your changes instantly.')
-})
+  expect(saveChangesListItem.textContent).toBe(
+    'Save and see your changes instantly.',
+  );
+});
